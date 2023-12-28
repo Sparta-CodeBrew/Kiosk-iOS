450c65d5dd5511598cfa96c22fab8229bbf92132
TodoListViewController <gnasus1117@gmail.com>
2023-12-28 17:48:51 +0900

    메뉴화면구성

---
+++ CodeBrew/CodeBrew/MenuList/MenuListView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuListView.swift
+//  CodeBrew
+//
+//  Created by t2023-m0039 on 12/28/23.
+//
+
+import UIKit
+
+class MenuListView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CodeBrew/CodeBrew/MenuList/Model/MenuListStruct.swift
@@ -0,0 +1,12 @@
+//
+//  MenuListStruct.swift
+//  CodeBrew
+//
+//  Created by t2023-m0039 on 12/28/23.
+//
+
+import UIKit
+
+class MenuListStruct: UICollectionViewCell {
+    
+}
